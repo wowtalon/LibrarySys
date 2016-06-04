4d3993ffbb7bd9f92035409202d6a46112da83ba
zhuangshq <1120692265@qq.com>
2016-06-04 19:15:27 +0800

    0.0.1

--- LibrarySys/book.cpp
+++ LibrarySys/book.cpp
@@ -1,23 +1,36 @@
 #include "book.h"
 
 Book::Book() {
-
+	this->name = "";
+	this->author = "";
+	this->ISBN = "";
+	this->isAvailable = false;
 }
 Book::Book(const Book& b) {
-
+	this->name = b.getName;
+	this->author = b.getAuthor;
+	this->ISBN = b.getISBN;
+	this->isAvailable = b.getIsAvailable;
 }
-Book::Book(string name, string author, string ISBN) {
-
+Book::Book(string name, string author, string ISBN, bool b) {
+	this->name = name;
+	this->author = author;
+	this->ISBN = ISBN;
+	this->isAvailable = b;
 }
+
 bool Book::insert() {
 
 }
+
 bool Book::deleteByISBN(string ISBN) {
 
 }
+
 bool Book::borrow(string ISBN) {
 
 }
+
 void Book::findAll() {
 
 }
@@ -29,4 +42,20 @@ void Book::findByAuthor(string author) {
 }
 void Book::findByISBN(string ISBN) {
 
+}
+
+string Book::getName() {
+	return this->name;
+}
+
+string Book::getAuthor() {
+	return this->author;
+}
+
+string Book::getISBN() {
+	return this->ISBN;
+}
+
+bool Book::getIsAvailable() {
+	return this->isAvailable;
 }
--- LibrarySys/book.h
+++ LibrarySys/book.h
@@ -9,6 +9,7 @@ private:
 	string name;
 	string ISBN;
 	string author;
+	bool isAvailable;
 	vector<string> v_name;
 	vector<string> v_ISBN;
 	vector<string> v_author;
@@ -16,7 +17,7 @@ private:
 public:
 	Book();
 	Book(const Book& b);
-	Book(string name, string author, string ISBN);
+	Book(string name, string author, string ISBN, bool b);
 	bool insert();
 	bool deleteByISBN(string ISBN);
 	bool borrow(string ISBN);
@@ -24,6 +25,10 @@ public:
 	void findByName(string name);
 	void findByAuthor(string author);
 	void findByISBN(string ISBN);
+	string getName();
+	string getAuthor();
+	string getISBN();
+	bool getIsAvailable();
 };
 
 #endif
